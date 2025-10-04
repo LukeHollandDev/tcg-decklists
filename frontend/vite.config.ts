a72554821280ea58bb2106cfde620b35b24295ff
Luke Holland <lukeholland15500@gmail.com>
2025-10-04 18:28:34 +0100

    switched to base vite tempalte

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,15 @@
-import { defineConfig } from 'vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-const config = defineConfig({
+// https://vite.dev/config/
+export default defineConfig({
   plugins: [
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
     }),
     tailwindcss(),
-    tanstackStart(),
-    viteReact(),
   ],
-})
-
-export default config
+});
